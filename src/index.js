cc578186fa7f8772eaca10d017c859d8f7a1fbb2
Wesley Mendes <wesleymendes.dev@gmail.com>
2023-09-20 13:52:52 -0300

    Alterações nos nomes das variáveis

--- src/index.js
+++ src/index.js
@@ -1,32 +1,32 @@
 let nomeDoUsuario = 'Machine Gun Funk'
 
-let nivelDoUsuário = 3333
+let xpDoUsuario = 3333
 
 let nivel;
 
 switch(true){
-    case nivelDoUsuário <= 1000:
+    case xpDoUsuario <= 1000:
         nivel = 'Ferro';
         break
-    case nivelDoUsuário <= 2000:
+    case xpDoUsuario <= 2000:
         nivel = 'Bronze'
         break
-    case nivelDoUsuário <= 5000:
+    case xpDoUsuario <= 5000:
         nivel = 'Prata'
         break
-    case nivelDoUsuário <= 7000:
+    case xpDoUsuario <= 7000:
         nivel = 'Ouro'
         break
-    case nivelDoUsuário <= 8000:
+    case xpDoUsuario <= 8000:
         nivel = 'Platina'
         break
-    case nivelDoUsuário <= 9000:
+    case xpDoUsuario <= 9000:
         nivel = 'Ascendente'
         break
-    case nivelDoUsuário <= 10000:
+    case xpDoUsuario <= 10000:
         nivel = 'Imortal'
         break
-    case nivelDoUsuário >= 10001:
+    case xpDoUsuario >= 10001:
         nivel = 'Radiante'
         break
     default:
